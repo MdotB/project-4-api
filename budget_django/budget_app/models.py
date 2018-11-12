aae38f538610dd422d3839a51bd677f2c951b505
Mike Bell <michaelbell.va@gmail.com>
2018-11-12 14:40:15 -0500

    set up budget model and complete first migration

--- budget_django/budget_app/models.py
+++ budget_django/budget_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Budget(models.Model):
+    category = models.CharField(max_length=100)
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return self.category
+
+
+
